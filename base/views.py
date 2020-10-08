1269ed5cda606c20dd9cf945b9d80bee687defa7
nanthu0123 <nanthu0123@gmail.com>
2020-10-08 20:54:33 +0530

    user registration form created

--- base/views.py
+++ base/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm
+
 
 def home(request):
     return render(request, 'home.html')
 
 
 def register(request):
-    return render(request, 'register.html')
+    form = UserRegisterForm
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'register.html', context)
 
 
 def login(request):
